7a4b51faa192f4bdcd5e0e2497c4617050245a2f
Andriy Martynovskyj <andriy.martynovskyj@magefan.com>
2018-04-13 18:33:48 +0300

    commits

--- Plugin/Deploy/Package/Bundle/RequireJsPlugin.php
+++ Plugin/Deploy/Package/Bundle/RequireJsPlugin.php
@@ -5,17 +5,45 @@ use Magento\Deploy\Package\Bundle\RequireJs;
 
 class RequireJsPlugin
 {
-//    protected $scopeConfig;
-//
-//    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
-//    {
-//        $this->scopeConfig = $scopeConfig;
-//    }
+    protected $scopeConfig;
+
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
 
     public function aroundAddFile(RequireJs $subject, callable $proceed, $filePath, $sourcePath, $contentType) {
 
-//        $jsOptimization = $this->scopeConfig->getValue('mfrocketjavascript/enable_javaScript_bundling_optimization', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-//        var_dump($jsOptimization); exit();
+        $jsOptimization = $this->scopeConfig->getValue('mfrocketjavascript/general/enable_javaScript_bundling_optimization', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        if ($jsOptimization) {
+            $includeInBundling = $this->scopeConfig->getValue('mfrocketjavascript/general/included_in_bundling', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            $allowedFiles = str_replace("\r","\n", $includeInBundling);
+            $allowedFiles = explode("\n", $allowedFiles);
+
+            foreach ($allowedFiles  as $key => $allowedFile) {
+                $allowedFiles[$key] = trim($allowedFile);
+                if (empty($allowedFiles[$key])) {
+                    unset($allowedFiles[$key]);
+                }
+            }
+
+            foreach ($allowedFiles as $allowed) {
+                $allowedFiles[] = str_replace('.min.js', '.js', $allowed);
+            }
+
+            $include = false;
+            foreach ($allowedFiles as $allowedFile) {
+                if (strpos($sourcePath, $allowedFile) !== false) {
+                    $include = true;
+                    break;
+                }
+            }
+
+            if (!$include) {
+                return true;
+            }
+
+        }
         return $proceed($filePath, $sourcePath, $contentType);
     }
 }
